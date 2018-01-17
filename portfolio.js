1badbebd9edc61e4e7050f4449ffd5f2b338d71b
Tani W <taniw@Tanis-MacBook-Pro-2.local>
2018-01-17 10:58:58 -0800

    Test commit

--- portfolio.js
+++ portfolio.js
@@ -2,8 +2,15 @@
 $(document).ready(function() {
    
     $('h1').unbind().click(testing);
+    $('.rectangle').unbind().click(showWorkDetails);
 });
+
+
 
 function testing() {
 	console.log('does this work');
+}
+
+function showWorkDetails() {
+	$('#showWorkDiv').fadeIn();
 }
