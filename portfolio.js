3bd01056eff529314181fb6603537c4d62e71a45
Tani W <taniw@Tanis-MacBook-Pro-2.local>
2018-01-22 18:50:09 -0800

    Editing content details

--- portfolio.js
+++ portfolio.js
@@ -1,24 +1,105 @@
 
 $(document).ready(function() {
-   c
     //$('.rectangle').unbind().click(showWorkDetails);
 
     $('#rec1').unbind().click(showRec1);
     $('#rec2').unbind().click(showRec2);
+    $('#rec3').unbind().click(showRec3);
+    $('#rec4').unbind().click(showRec4);
+    $('#rec5').unbind().click(showRec5);
+    $('#rec6').unbind().click(showRec6);
+    $('#rec7').unbind().click(showRec7);
+    $('#rec8').unbind().click(showRec8);
+    $('#rec9').unbind().click(showRec9);
+    $('#rec10').unbind().click(showRec10);
+    $('#rec11').unbind().click(showRec11);
+    $('#rec12').unbind().click(showRec12);
+    $('#rec13').unbind().click(showRec13);
+    $('#rec14').unbind().click(showRec14);
+    $('#rec15').unbind().click(showRec15);
 });
 
 
-/*function showWorkDetails() {
-	$('#showWorkDiv').fadeIn();
-}*/
-
 function showRec1() {
-	//$('#rec2-content').hide();
-	$('#rec1-content').show();
+	$('.work-details').hide();
+	$('#rec1-content').fadeIn();
 }
 
 function showRec2() {
-	console.log('asdfadf');
-	$('#rec1-content').hide();
-	$('#rec2-content').show();
+	$('.work-details').hide();
+	$('#rec2-content').fadeIn();
 }
+
+function showRec3() {
+	$('.work-details').hide();
+	$('#rec3-content').fadeIn();
+}
+
+function showRec4() {
+	$('.work-details').hide();
+	$('#rec4-content').fadeIn();
+}
+
+function showRec5() {
+	$('.work-details').hide();
+	$('#rec5-content').fadeIn();
+}
+
+function showRec6() {
+	$('.work-details').hide();
+	$('#rec6-content').fadeIn();
+}
+
+function showRec7() {
+	$('.work-details').hide();
+	$('#rec7-content').fadeIn();
+}
+
+function showRec8() {
+	$('.work-details').hide();
+	$('#rec8-content').fadeIn();
+}
+
+function showRec9() {
+	$('.work-details').hide();
+	$('#rec9-content').fadeIn();
+}
+
+function showRec10() {
+	$('.work-details').hide();
+	$('#rec10-content').fadeIn();
+}
+
+function showRec11() {
+	$('.work-details').hide();
+	$('#rec11-content').fadeIn();
+}
+
+function showRec12() {
+	$('.work-details').hide();
+	$('#rec12-content').fadeIn();
+}
+
+function showRec13() {
+	$('.work-details').hide();
+	$('#rec13-content').fadeIn();
+}
+
+function showRec14() {
+	$('.work-details').hide();
+	$('#rec14-content').fadeIn();
+}
+
+function showRec15() {
+	$('.work-details').hide();
+	$('#rec15-content').fadeIn();
+}
+
+
+
+
+
+
+
+
+
